f0f066a5ad111ea9fbb3a03881f8d146a6c102d8
Nipun Arora <aroranipun1@gmail.com>
2024-04-06 13:39:11 +0530

    Added css stylesheets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css'
+import './css/UserList.css'
+import './css/LoadingSpinner.css'
+import './css/UserForm.css'
 import App from './App';
 
 ReactDOM.render(
